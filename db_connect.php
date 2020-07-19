38cb5a316ee967b9292855d0e8729a8db4e794a7
Mauro Silvino <67653716+MauroSilvino@users.noreply.github.com>
2020-07-18 23:46:43 -0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "jardim197";
+$password = "";
 $db_name = "sistemalogin";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
